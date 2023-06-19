34280efbe65156e225b7c59be6cb8b590d35577a
jack dason <dasjack@outlook.com>
2023-06-19 11:49:03 +0800

    reconstruct

--- index.js
+++ index.js
@@ -1,89 +1,53 @@
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+// const { init: initDB, Counter } = require("./db");
 const http = require('node:https');
-
 const logger = morgan("tiny");
-
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(logger);
-
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-  // res.json(await postTest());
-});
-
-
-
-// 获取
-app.get("/api/model", async (req, res) => {
-  const result = await postJson('www.wenxinkejian.com','/api/model');
-  
-  res.send(result);
-});
-
-app.post("/api/draw", async (req, res) => {
-
-  const result = await postJson('www.wenxinkejian.com','/api/draw',req.body);
-  res.send(result);
-});
-app.post("/api/work", async (req, res) => {
-
-  const result = await postJson('www.wenxinkejian.com','/api/work',req.body);
-  res.send(result);
-});
-
-
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    // res.send(req.headers["x-wx-openid"]);
-    res.json({
-      openid:req.headers["x-wx-openid"]
-    });
-  }
-});
-
-const port = process.env.PORT || 80;
-
+/**
+ * 
+ * @param {*} options 
+ * @param {*} requestData 
+ * @returns 
+ */
 function requestAsync(options, requestData){
   return new Promise((resolve, reject) => {
     try{
-
       const req = http.request(options, (res) => {
         const chunks = [];
-  
         res.on('data', (chunk) => {
           chunks.push(chunk);
-        });
-  
+        });  
         res.on('end', () => {
           const responseBody = Buffer.concat(chunks).toString();
           resolve({ responseBody, responseHeaders: res.headers });
         });
-      });
-  
+      });  
       req.on('error', (err) => {
         reject(err);
       });
-  
       if (requestData) {
         req.write(requestData);
       }
-  
       req.end();
     }catch(e){
       reject(e);
     }
-   
   });
 }
+/**
+ * 
+ * @param {*} base 
+ * @param {*} path 
+ * @param {*} obj 
+ * @returns 
+ */
 async function postJson(base,path,obj){
   const data=obj&&JSON.stringify(obj);
   const options = {
@@ -95,11 +59,17 @@ async function postJson(base,path,obj){
       'Content-Length': Buffer.byteLength(data||''),
     },
   };
-
   const result= await requestAsync(options,data);
   return result.responseBody;
 }
-async function getJson(base,path,obj){
+/**
+ * 
+ * @param {*} base 
+ * @param {*} path 
+ * @param {*} obj 
+ * @returns 
+ */
+async function _getJson(base,path,obj){
   
   if(obj){
     let data= '';
@@ -113,63 +83,100 @@ async function getJson(base,path,obj){
     path: path,
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Length': Buffer.byteLength(postData),
+      'Content-Type': 'application/json'
     },
   };
-
   const result= await requestAsync(options,data);
   return result.responseBody;
 }
-async function postTest() {
-  
-  const postData = JSON.stringify({
-    
-      username: "admin",
-      password: "admin(*)!@34\"}"
-    
-  });
 
-  const options = {
-    hostname: 'www.wenxinkejian.com',
-    // port: 43,
-    path: '/api/login',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData),
-    },
-  };
+// // 首页
+// app.get("/", async (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+//   // res.json(await postTest());
+// });
 
-  var result = await requestAsync(options,postData);
-  return result;
-  // const req = http.request(options, (res) => {
-  //   console.log(`STATUS: ${res.statusCode}`);
-  //   console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  //   res.setEncoding('utf8');
-  //   res.on('data', (chunk) => {
-  //     console.log(`BODY: ${chunk}`);
-  //   });
-  //   res.on('end', () => {
-  //     console.log('No more data in response.');
+/**
+ * 获取模板库
+ */
+app.get("/api/model", async (req, res) => {
+  const result = await postJson('www.wenxinkejian.com','/api/model',req.body);
+  res.send(result);
+});
+/**
+ * 绘制
+ * return:{drawid:}
+ */
+app.post("/api/draw", async (req, res) => {
 
-  //   });
-  // });
+  const result = await postJson('www.wenxinkejian.com','/api/draw',req.body);
+  res.send(result);
+});
+/**
+ * 查询绘制结果
+ * req:
+ * openid
+ * drawid
+ * res:
+ *  {
+        "id": 17,
+        "uid": 10004,
+        "image_id": 5,
+        "operation": 5,
+        "execution": "u_4",
+        "image_addr": "8b05a5fac291d3d70e23f51ad9ca82cc.jpg",
+        "image_prompt": "",
+        "mid_res_data": {
+            "id": "1118023017562382406",
+            "uri": "https://cdn.discordapp.com/attachments/1105706108011696251/1118023017239425024/tomato-h_a_handsome_young_manprotraitghibli_style_illustration__03d9710b-c54a-4961-aa43-0960b052c639.png",
+            "hash": "03d9710b-c54a-4961-aa43-0960b052c639",
+            "content": "<https://s.mj.run/pnNYvMz_YII> a handsome young man,protrait,ghibli style, illustration style,clean background,--iw 1.5 --niji 5 --seed 3864 --q 0.5 --niji 5",
+            "progress": "done"
+        },
+        "image_convert_addr": "u_4_8b05a5fac291d3d70e23f51ad9ca82cc.jpg",
+        "greate_time": "2023-06-13T11:44:21+08:00",
+        "update_time": "2023-06-13T11:44:21+08:00"
+    }
+ */
+app.post("/api/work", async (req, res) => {
+  //
+  const result = await postJson('www.wenxinkejian.com','/api/work',req.body);
+  res.send(result);
+});
+/**
+ * 获取所有已绘制结果
+ * req:
+ * openid
+ * 
+ * res:
+ * 
+ */
+app.post("/api/works", async (req, res) => {
+  //
+  const result = await postJson('www.wenxinkejian.com','/api/works',req.body);
+  res.send(result);
+});
 
-  // req.on('error', (e) => {
-  //   console.error(`problem with request: ${e.message}`);
-  // });
+/**
+ * 小程序调用，获取微信 Open ID
+ */
+app.get("/api/wx_openid", (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    res.json({
+      openid:req.headers["x-wx-openid"]
+    });
+  }
+});
 
-  // // Write data to request body
-  // req.write(postData);
-  // req.end()
-}
-async function bootstrap() {
+
+
+/**
+ * 
+ */
+function bootstrap(port) {
   // await initDB();
-
   app.listen(port, () => {
     console.log("启动成功", port);
   });
 }
-
-bootstrap();
+bootstrap(process.env.PORT || 80);
